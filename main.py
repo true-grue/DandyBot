8f35623fb0072795438c12dd492b6d864b4a395a
Peter Sovietov <peter.sovietov@gmail.com>
2021-01-06 17:54:35 +0300

    new levels

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-﻿import sys
-import time
+﻿import time
+import sys
 import json
 from importlib import import_module
 from pathlib import Path
@@ -8,7 +8,7 @@ import tkinter as tk
 from plitk import load_tileset, PliTk
 
 SCALE = 1
-DELAY = 100
+DELAY = 50
 
 UP = "up"
 DOWN = "down"
@@ -30,50 +30,45 @@ class Board:
         self.tileset = load_tileset(game["tileset"])
         self.screen = PliTk(canvas, 0, 0, 0, 0, self.tileset, SCALE)
         self.load_players()
-        self.current_round = 0
+        self.level_index = 0
         self.load_level()
 
     def load_players(self):
         self.players = []
         for i, name in enumerate(self.game["players"]):
-            m = import_module(name)
+            script = import_module(name).script
             tile = self.game["tiles"]["@"][i]
-            self.players.append(Player(name, m.script, self, tile))
+            self.players.append(Player(name, script, self, tile))
         shuffle(self.players)
 
     def load_level(self):
+        self.gold = 0
         self.steps = 0
-        data = self.game["levels"][self.game["rounds"][self.current_round]]
+        self.level = self.game["levels"][self.level_index]
+        data = self.game["maps"][self.level["map"]]
         cols, rows = len(data[0]), len(data)
-        self.canvas.config(
-            width=cols * self.tileset["tile_width"] * SCALE,
-            height=rows * self.tileset["tile_height"] * SCALE)
-        self.screen.resize(cols, rows)
-        self.level = [[data[y][x] for y in range(rows)] for x in range(cols)]
+        self.map = [[data[y][x] for y in range(rows)] for x in range(cols)]
         self.has_player = [[None for y in range(rows)] for x in range(cols)]
-        player_x, player_y = 0, 0
-        self.cost = 0
+        self.canvas.config(width=cols * self.tileset["tile_width"] * SCALE,
+                           height=rows * self.tileset["tile_height"] * SCALE)
+        self.screen.resize(cols, rows)
         for y in range(rows):
             for x in range(cols):
-                if self.level[x][y] == "@":
-                    player_x, player_y = x, y
-                    self.level[x][y] = " "
-                self.cost += self.check("gold", x, y)
                 self.update(x, y)
         for p in self.players:
-            self.add_player(p, player_x, player_y)
+            self.add_player(p, *self.level["start"])
         self.update_score()
 
     def get(self, x, y):
         if x < 0 or y < 0 or x >= self.screen.cols or y >= self.screen.rows:
             return "#"
-        return self.level[x][y]
+        return self.map[x][y]
 
     def update(self, x, y):
         if self.has_player[x][y]:
             self.screen.set_tile(x, y, self.has_player[x][y].tile)
         else:
-            self.screen.set_tile(x, y, self.game["tiles"][self.level[x][y]])
+            self.screen.set_tile(x, y, self.game["tiles"][self.map[x][y]])
 
     def remove_player(self, player):
         self.has_player[player.x][player.y] = None
@@ -85,12 +80,15 @@ class Board:
         self.update(x, y)
 
     def take_gold(self, x, y):
-        self.cost -= self.check("gold", x, y)
-        self.level[x][y] = " "
+        self.gold += self.check("gold", x, y)
+        self.map[x][y] = " "
         self.update(x, y)
         self.update_score()
 
-    def check(self, cmd, x, y):
+    def check(self, cmd, *args):
+        if cmd == "level":
+            return self.level_index + 1
+        x, y = args
         item = self.get(x, y)
         if cmd == "wall":
             return item == "#"
@@ -102,28 +100,24 @@ class Board:
     def play(self):
         for p in self.players:
             p.act(p.script(self.check, p.x, p.y))
+        if self.gold >= self.level["gold"]:
+            return self.next_level()
         self.steps += 1
+        return self.steps < self.level["steps"]
 
     def update_score(self):
-        if self.current_round < len(self.game["rounds"]):
-            lines = [("Round:%4d\n" % (self.current_round + 1))]
-        else:
-            lines = ["GAME OVER!\n"]
-        players = sorted(self.players, key=lambda x: x.cost, reverse=True)
+        lines = [("Level:%4d\n" % (self.level_index + 1))]
+        players = sorted(self.players, key=lambda x: x.gold, reverse=True)
         for p in players:
-            lines.append("%s:%4d" % (p.name, p.cost))
+            lines.append("%s:%4d" % (p.name, p.gold))
         self.label["text"] = "\n".join(lines)
 
-    def next_round(self):
-        self.current_round += 1
-        if self.current_round < len(self.game["rounds"]):
+    def next_level(self):
+        self.level_index += 1
+        if self.level_index < len(self.game["levels"]):
             self.load_level()
             return True
-        self.update_score()
         return False
-
-    def is_game_ended(self):
-        return self.steps >= self.game["steps"] or self.cost == 0
 
 
 class Player:
@@ -133,7 +127,7 @@ class Player:
         self.board = board
         self.tile = tile
         self.x, self.y = 0, 0
-        self.cost = 0
+        self.gold = 0
 
     def act(self, cmd):
         dx, dy = 0, 0
@@ -158,21 +152,20 @@ class Player:
         board.add_player(self, self.x, self.y)
 
     def take(self):
-        board = self.board
-        cost = board.check("gold", self.x, self.y)
-        if cost > 0:
-            board.take_gold(self.x, self.y)
-            self.cost += cost
+        gold = self.board.check("gold", self.x, self.y)
+        if gold:
+            self.gold += gold
+            self.board.take_gold(self.x, self.y)
 
 
 def start_game():
     def update():
         t = time.time()
-        board.play()
-        dt = int((time.time() - t) * 1000)
-        if board.is_game_ended() and not board.next_round():
-            return
-        root.after(max(DELAY - dt, 0), update)
+        if board.play():
+            dt = int((time.time() - t) * 1000)
+            root.after(max(DELAY - dt, 0), update)
+        else:
+            label["text"] += "\n\nGAME OVER!"
 
     root = tk.Tk()
     root.configure(background="black")
@@ -181,7 +174,8 @@ def start_game():
     label = tk.Label(root, font=("TkFixedFont",),
                      justify=tk.RIGHT, fg="white", bg="gray20")
     label.pack(side=tk.RIGHT, anchor="n")
-    game = json.loads(Path("game.json").read_text())
+    filename = sys.argv[1] if len(sys.argv) == 2 else "game.json"
+    game = json.loads(Path(filename).read_text())
     board = Board(game, canvas, label)
     root.after(0, update)
     root.mainloop()
---
+++ user_bot.py
@@ -0,0 +1,8 @@
+﻿import random
+import random_bot
+
+
+def script(check, x, y):
+    if check("level") == 1:
+        return random_bot.script(check, x, y)
+    return "pass"

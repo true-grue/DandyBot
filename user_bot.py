5e5588438bbb6c6a5ca68c652ddab4cf0a96a4de
GrigorUN <155751051+amazonooo@users.noreply.github.com>
2024-02-28 19:19:16 +0300

    2 lvl

--- user_bot.py
+++ user_bot.py
@@ -1,4 +1,22 @@
 ﻿def script(check, x, y):
-    if check("level") == 1:
+    if check('gold', x  , y):
+            return "take"
+    if check('gold', x + 1, y):
+            return "right"
+    if check('gold', x - 1, y):
+            return "left"
+    if check('gold', x, y + 1):
+            return "down"
+    if check('gold', x, y - 1):
+            return "up"
+        
+    elif check("level") == 1:
         return "right"
+    
+    elif check("level") == 2:
+        if check("gold", x + 2, y):
+            return "right"
+        return "up"
+    
+        
     return "pass"
